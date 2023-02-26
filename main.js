d9544a119c035086bc2d09640822f22b791a1844
rarira2010 <rarira2010@gmail.com>
2023-02-26 14:16:11 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-function check() {
-  var element=document.getElementById('number');
-  var n=BigInt(element.value);
-  if(n<2n){alert("これはそすうじゃないです。そうっすね〜");return;}
-  if(n==2n){alert("これは素数ですね");return;}
-  if(n%2n==0n){alert("これはそすうじゃないです。そうっすね〜");return;}
-  for (var i = BigInt(3); i*i <=n;i++){if(n%i==0){alert("これはそすうじゃないです。そうっすね〜");return;}}alert("これは素数ですね");return;}
+function check() {var element=document.getElementById('number');var n=BigInt(element.value);if(n<2n){alert("これはそすうじゃないです。そうっすね〜");return;}if(n==2n){alert("これは素数ですね");return;}if(n%2n==0n){alert("これはそすうじゃないです。そうっすね〜");return;}for(var i = BigInt(3); i*i <=n;i++){if(n%i==0){alert("これはそすうじゃないです。そうっすね〜");return;}}alert("これは素数ですね");return;}
